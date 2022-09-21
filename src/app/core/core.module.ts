ab82680b8543ac67a7975933822f80246beb2a8c
a rahni <rahniahmed@gmail.com>
2022-09-21 17:10:07 +0200

    initilize project angular crm, architecture

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [CommonModule],
+  exports: [IconsModule, LoginModule, TemplatesModule, UiModule, HeaderComponent, NavComponent, FooterComponent],
+})
+export class CoreModule {}
---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Ui2Component } from './components/ui2/ui2.component';
+import { UiComponent } from './components/ui/ui.component';
+
+@NgModule({
+  declarations: [Ui2Component, UiComponent],
+  imports: [CommonModule],
+  exports: [Ui2Component, UiComponent],
+})
+export class UiModule {}
