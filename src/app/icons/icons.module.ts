0b688ca6279aca34bd27a4634254b18e3e2fcaef
a rahni <rahniahmed@gmail.com>
2022-09-22 14:03:52 +0200

    open-close nav -create icon components

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    IconCloseComponent,
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    IconCloseComponent,
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent,
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
